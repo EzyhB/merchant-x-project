63330fb693fd001d1e39fde7e685f11622c544dc
EzyhB <ezyhbmail@gmail.com>
2022-04-05 15:14:50 +0100

    added Container, GridContainer and globalstyle/d.ts file

--- src/components/styles/GridContainer/index.tsx
+++ src/components/styles/GridContainer/index.tsx
@@ -1,1 +1,10 @@
-export {};
+import styled from "styled-components";
+
+const GridContainer = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-flow: row wrap;
+  width: 100%;
+`;
+
+export { GridContainer };
