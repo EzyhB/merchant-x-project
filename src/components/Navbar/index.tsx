4af438c7fa050810f80e0ea473379b83f63bda16
EzyhB <ezyhbmail@gmail.com>
2022-04-05 17:33:31 +0100

    navbar added

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,1 +1,55 @@
-export {};
+import { Container } from "../styles/Container";
+import { GridContainer } from "../styles/GridContainer";
+import { GridItem } from "../styles/GridItem";
+
+import css from "./Navbar.module.css";
+// import darkLogo from "./images/darkLogo.png";
+// import lightLogo from "./images/lightLogo.png";
+
+interface Props {
+  isLight: boolean;
+  setIsLight: Function;
+}
+
+export default function Navbar({ isLight, setIsLight }: Props) {
+  let themeSwitchBackground = isLight ? "#28293E" : "#ffffff";
+  return (
+    <Container maxWidth="none">
+      <GridContainer>
+        <GridItem md="four" xs="three">
+          <img
+            className={css.mdLogo}
+            src={isLight ? "./images/lightLogo.png" : "./images/darkLogo.png"}
+            alt="merchant x logo"
+          />
+        </GridItem>
+        <GridItem className={css.xsLogoGrid} md="four" xs="six">
+          <img
+            className={css.xsLogo}
+            src={isLight ? "./images/lightLogo.png" : "./images/darkLogo.png"}
+            alt="merchant x logo"
+          />
+        </GridItem>
+        <GridItem className={css.themeSwitchGrid} md="four" xs="three">
+          <div
+            style={{ background: themeSwitchBackground }}
+            className={css.themeSwitchBackground}
+            onClick={() => {
+              setIsLight(!isLight);
+            }}
+          >
+            <img
+              className={css.themeSwitch}
+              src={
+                isLight
+                  ? "./images/darkModeIcon.png"
+                  : "./images/lightModeIcon.png"
+              }
+              alt="theme switch button"
+            />
+          </div>
+        </GridItem>
+      </GridContainer>
+    </Container>
+  );
+}
